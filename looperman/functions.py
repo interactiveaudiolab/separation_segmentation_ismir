b3fa2413fd31c82aa7fe99fb5de3401ed74dd812
Prem Seetharaman <prem@u.northwestern.edu>
2016-06-20 11:47:48 -0700

    adding files

---
+++ looperman/functions.py
@@ -0,0 +1,186 @@
+import numpy as np
+import librosa
+from nussl import Repet, RepetType, AudioSignal
+from sklearn.decomposition import NMF
+import peakutils
+from scipy.spatial.distance import cosine
+
+def find_template(music_stft, sr, min_t, n_components, start, end):
+	template_stft = music_stft[:, start:end]
+	layer = librosa.istft(template_stft)
+	layer_rms = np.sqrt(np.mean(layer*layer))
+
+	comps = []
+	acts = []
+	errors = []
+	
+	for T in range(min_t, n_components):
+		transformer = NMF(n_components = T)
+		comps.append(transformer.fit_transform(np.abs(template_stft)))
+		acts.append(transformer.components_)
+		errors.append(transformer.reconstruction_err_)
+	
+
+	#knee = np.diff(errors, 2)
+	#knee = knee.argmax() + 2
+	knee = 0
+	#print 'Using %d components' % (knee + min_t)
+	return comps[knee], acts[knee]
+
+def find_template_knee(music_stft, sr, min_t, n_components, start, end):
+	template_stft = music_stft[:, start:end]
+	layer = librosa.istft(template_stft)
+	layer_rms = np.sqrt(np.mean(layer*layer))
+
+	comps = []
+	acts = []
+	errors = []
+	
+	for T in range(min_t, n_components):
+		transformer = NMF(n_components = T)
+		comps.append(transformer.fit_transform(np.abs(template_stft)))
+		acts.append(transformer.components_)
+		errors.append(transformer.reconstruction_err_)
+	
+
+	knee = np.diff(errors, 2)
+	knee = knee.argmax() + 2
+	print 'Using %d components' % (knee + min_t)
+	return comps[knee], acts[knee]
+
+def extract_template(comps, music_stft):
+	K = comps.shape[1]
+	
+	#initialize transformer (non-negative matrix factorization) with K components
+	transformer = NMF(n_components = K, init = 'custom')
+	
+	#W and H are random at first
+	W = np.random.rand(comps.shape[0], K)
+	H = np.random.rand(K, music_stft.shape[1])
+	
+	#set W to be the template components you want to extract
+	W[:, 0:K] = comps
+
+	#don't let W get updated in the non-negative matrix factorization
+	params = {'W': W, 'H': H, 'update_W': False}
+	comps_music = transformer.fit_transform(np.abs(music_stft), **params)
+	acts_music = transformer.components_
+	
+	#reconstruct the signal
+	music_reconstruction = comps_music.dot(acts_music)
+
+	#mask the input signal
+	music_stft_max = np.maximum(music_reconstruction, np.abs(music_stft))
+	mask = np.divide(music_reconstruction, music_stft_max)
+	mask = np.nan_to_num(mask)
+	
+	#binary mask
+	mask = np.round(mask)
+
+	#template - extracted template, residual - everything that's leftover.
+	template = np.multiply(music_stft, mask)
+	residual = np.multiply(music_stft, 1 - mask)
+
+	return template, residual
+
+def extract_reconstruction_errors(comps, music_stft, window_length, hop):
+	K = comps.shape[1]
+	#initialize transformer (non-negative matrix factorization) with K components
+	transformer = NMF(n_components = K, init = 'custom')
+	#W and H are random at first
+	W = np.random.rand(comps.shape[0], K)
+	start = 0
+	errors = []
+
+	while (start + window_length < music_stft.shape[1]):
+		block = music_stft[:, start:start+window_length]
+		
+		H = np.random.rand(K, block.shape[1])
+		W[:, 0:K] = comps
+		
+		params = {'W': W, 'H': H, 'update_W': False}
+		comps_block = transformer.fit_transform(np.abs(block), **params)
+		acts_block = transformer.components_
+	
+		#reconstruct the signal
+		block_reconstruction = comps_block.dot(acts_block)
+		errors.append(transformer.reconstruction_err_)
+
+		start = start + hop
+	return errors
+
+from numpy.linalg import norm
+
+def extract_reconstruction_error_beats(comps, music_stft, beats):
+	K = comps.shape[1]
+	#initialize transformer (non-negative matrix factorization) with K components
+	transformer = NMF(n_components = K, init = 'custom')
+	#W and H are random at first
+	W = np.random.rand(comps.shape[0], K)
+	start = 0
+	errors = []
+	lookback = 0
+	weight = np.array([1 for i in range(2, music_stft.shape[0] + 2)])
+	weight = weight/np.max(weight)
+	for i in range(lookback+1, len(beats)):
+		block = music_stft[:, beats[i-(lookback+1)]:beats[i]]
+		
+		H = np.random.rand(K, block.shape[1])
+		W[:, 0:K] = comps
+		
+		params = {'W': W, 'H': H, 'update_W': False}
+		comps_block = transformer.fit_transform(np.abs(block), **params)
+		acts_block = transformer.components_
+
+		#reconstruct the signal
+		block_reconstruction = comps_block.dot(acts_block)
+		
+		block_reconstruction = block_reconstruction.T*weight
+		block = block.T*weight
+		distance = norm(block_reconstruction - np.abs(block))
+		#errors.append(transformer.reconstruction_err_)
+		errors.append(distance)
+	return errors
+
+def get_beat_spectrum(music):
+	audio = AudioSignal(audio_data_array = music)
+	r = Repet(audio_signal = audio, repet_type = RepetType.DEFAULT)
+	r.run()
+	b = r.get_beat_spectrum()
+	b = np.square(b/b[0])
+	return b
+
+def get_beats(beat_spectrum, music, sr, rate):
+	length_in_seconds = float(len(music))/sr
+	conversion = length_in_seconds/len(beat_spectrum)
+
+	auto_cosine = np.zeros((len(beat_spectrum), 1))
+	for i in range(0, len(beat_spectrum) - 1):
+		auto_cosine[i] = 1 - cosine(beat_spectrum[0:len(beat_spectrum) - i], beat_spectrum[i:len(beat_spectrum)])
+	ac = auto_cosine[0:np.floor(auto_cosine.shape[0])/2]
+	auto_cosine = np.vstack([ac[1], ac, ac[-2]])
+	auto_cosine_diff = np.ediff1d(auto_cosine)
+	sign_changes = auto_cosine_diff[0:-1]*auto_cosine_diff[1:]
+	sign_changes = np.where(sign_changes < 0)[0]
+	
+	extrema_values = ac[sign_changes]
+
+	e1 = np.insert(extrema_values, 0, extrema_values[0])
+	e2 = np.insert(extrema_values, -1, extrema_values[-1])
+	
+	extrema_neighbors = np.stack((e1[0:-1], e2[1:]))
+	
+	m = np.amax(extrema_neighbors, axis=0)
+	extrema_values = extrema_values.flatten() 
+	maxima = np.where(extrema_values >= m)[0]
+	maxima = zip(sign_changes[maxima], extrema_values[maxima])
+	maxima = maxima[1:]
+	maxima = sorted(maxima, key = lambda x: -x[1])
+	period = maxima[0][0]
+	beats = np.arange(0, length_in_seconds, (period/float(rate))*conversion)
+	return period, beats
+
+def window_rms(a, window_size):
+	a2 = np.power(a, 2)
+	window = np.ones(window_size)/float(window_size)
+	return np.sqrt(np.convolve(a2, window, 'valid'))
---
+++ looperman/separate.py
@@ -0,0 +1,154 @@
+from functions import *
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+import peakutils
+from scipy.spatial.distance import cosine
+from scipy.signal import correlate
+from scipy.stats import skew
+
+def load_file(path):
+	music, sr = librosa.load(path)
+	return music, sr, librosa.stft(music)
+
+def segment_into_beats(music, sr):
+	#librosa beat track
+	tempo, beats = librosa.beat.beat_track(music)
+	more_beats = []
+	interpolation_factor = 2
+	for i in range(0, len(beats) - 1):
+		diff = np.floor((beats[i+1] - beats[i])/interpolation_factor)
+		for j in range(0, interpolation_factor):
+			more_beats.append(beats[i] + j*diff)
+	more_beats.append(beats[-1])
+	beats = np.array(more_beats)
+	
+	#or beat spectrum beat track
+	#b = get_beat_spectrum(music, sr)
+	#beats = get_beats(b, music, sr, 2)[1]
+	#beats = librosa.time_to_frames(beats)
+	music_stft = librosa.stft(music)
+	return music_stft, beats
+
+def beat_sync_error(music_stft, beats, sr):
+	comps, acts = find_template(music_stft, sr, 8, 9, beats[0], beats[7])
+	errors = extract_reconstruction_error_beats(comps, music_stft, beats)
+	return errors
+
+def get_template_error(music_stft, beats, comps):
+	errors = extract_reconstruction_error_beats(comps, music_stft, beats)
+	return errors
+
+from scipy.signal import wiener
+
+def mad(data):
+	return np.median(np.abs(data - np.median(data)))
+
+def find_inflection_point(errors, beats, music_stft, start, parameters):
+	errors = np.square(errors)
+	max_diff = np.max(np.abs(np.diff(errors, n=1)))
+	lag = parameters['lag']
+	p = parameters['p']
+	q = parameters['q']
+	means = []
+	stds = []
+	candidates = []
+	d = lag
+	while d < len(errors):
+		#weighted = [np.array(errors[d-i]) for i in range(1, lag)]
+		
+		window = errors[d-lag:d-1]
+		if np.abs(errors[d] - np.median(window)) > p*mad(window) and np.abs(errors[d] - errors[d-1]) > q*max_diff:
+			candidates.append(d)
+			d = d + lag
+		else:
+			d = d + 1
+		'''
+		if not means:
+			means.append(np.mean(weighted))
+			stds.append(np.std(weighted))
+		if (np.abs(errors[d] - np.max(weighted))) > diff*stds[-1] and errors[d] >= threshold:
+			print d, errors[d], threshold, means[-1], stds[-1]
+			means.append((means[-1] + influence*errors[d])/(1 + influence))
+			stds.append((stds[-1] + influence*np.sqrt((errors[d] - means[-1])**2))/(1 + influence))
+			candidates.append(d)
+		else:
+			means.append(np.mean(weighted))
+			stds.append(np.std(weighted))
+		'''
+	if not candidates:
+		candidates = [len(errors) - 1]
+	peak = candidates[0]
+	#print 'Going to beat %d - %f seconds' % (peak + start, librosa.frames_to_time(beats[peak])[0])
+	return beats[peak] - 4, peak + start, candidates
+	
+
+from scipy.spatial.distance import cosine
+
+def frame_similarity(i, j, beats, music_stft):
+	beat_i = np.abs(music_stft[:,beats[i]:beats[i+1]])
+	beat_j = np.abs(music_stft[:,beats[j]:beats[j+1]])
+	beat_i = np.max(beat_i, axis=1)
+	#beat_i = beat_i/np.max(beat_i)
+	beat_j = np.max(beat_j, axis=1)
+	#beat_j = beat_j/np.max(beat_j)
+	
+	#c = correlate(beat_i, beat_j, mode='full')
+	#return np.abs((np.argmax(c) - len(c)/2)/(float(len(c))/2))
+	return cosine(beat_i, beat_j)
+
+def frame_similarity_matrix(music_stft, beats):
+	matrix = np.zeros((len(beats), len(beats)))
+	for i in range(0, len(beats) - 1):
+		for j in range(i, len(beats) - 1):
+			matrix[i][j] = frame_similarity(i, j, beats, music_stft)
+	return (matrix + matrix.T)/2
+
+def find_first_beat_with_activity(music, sr, beats, start):
+	beats = librosa.frames_to_time(beats)
+	music = music[beats[start]*sr:]
+	total_rms = np.sqrt(np.mean(music*music))
+	for i, b in enumerate(beats[start:]):
+		sig = music[beats[i]*sr:beats[i+1]*sr]
+		rms = np.sqrt(np.mean(sig*sig))
+		#print i, b, rms, total_rms/20
+		if rms > total_rms/20:
+			break
+	return i + start
+
+def get_layer(music, sr, start, beats=None, parameters=None):
+	if beats is not None:
+		music_stft = librosa.stft(music)
+	else:
+		music_stft, beats = segment_into_beats(music, sr)
+	if parameters is None:
+		parameters = {'p': 5.5, 'q': .25, 'lag': 16}
+	start = find_first_beat_with_activity(music, sr, beats, start)
+	errors = beat_sync_error(music_stft, beats[start:], sr)
+	inflection_point, beat = find_inflection_point(errors, beats[start:], music_stft, start, parameters)
+	print 'Going from beat %d to %d' % (start, beat)
+	comps, acts = find_template(music_stft, sr, 8, 9, beats[start], inflection_point)
+	template, residual = extract_template(comps, music_stft)
+	template_error = get_template_error(music_stft, beats, comps)
+	return template, residual, errors, beats, inflection_point, beat, template_error, start
+
+def extract_all_layers(music_path, parameters=None):
+	music, sr, music_stft = load_file(music_path)
+	original_rms = np.sqrt(np.mean(music*music))
+	layers = []
+	boundaries = []
+	template, residual, errors, beats, inflection_point, beat, template_error, start = get_layer(music, sr, 0, parameters=parameters)
+	while True:
+		layers.append(librosa.istft(template))
+		boundaries.append(beats[beat])
+		print 'LAYER: ' + str(len(layers))
+		if np.sqrt(np.mean(music*music)) < original_rms/10:
+			print 'Residual rms too low, terminating'
+			break
+		if beat >= len(beats)-8:
+			print 'Went to end of file, terminating'
+			break
+		music = librosa.istft(residual)
+		start = beat
+		template, residual, errors, beats, inflection_point, beat, template_error, start = get_layer(music, sr, start, beats, parameters=parameters)
+	return layers, boundaries, music_stft
